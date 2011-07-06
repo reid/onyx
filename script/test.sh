10a895f88999036450791bbd485be65c915ea6de
Reid Burke <me@reidburke.com>
2011-07-06 13:33:26 -0700

    Use npm 1.0.x, yui3-bare ~0.6.2.

--- script/test.sh
+++ script/test.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 testdir="test"
-vows=`which vows 2>&1`
-ret=$?
-if [ $ret -ne 0 ] || ! [ -x $vows ]; then
+vows="./node_modules/vows/bin/vows"
+[ ! -d $testdir ] && cd ..
+if ! [ -x $vows ]; then
     echo "Unable to run Vows." >&2
     echo "Try \`make install-dev\` to install Vows." >&2
     exit $ret
 fi
-[ ! -d $testdir ] && cd ..
 [ ! -e $testdir ] && \
     echo "Unable to find directory '$testdir'." >&2 && exit 1
-vows $@ $testdir/*.js
+$vows $@ $testdir/*.js
