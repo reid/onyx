e0cf818512cd09b0370caefb35dfbfa9217b973f
Reid Burke <me@reidburke.com>
2011-07-06 15:06:29 -0700

    Add ./vows script to invoke Vows.

--- script/test.sh
+++ script/test.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-testdir="test"
 vows="./node_modules/vows/bin/vows"
-[ ! -d $testdir ] && cd ..
-if ! [ -x $vows ]; then
+[ ! -x $vows ] && cd ..
+if [ ! -x $vows ]; then
     echo "Unable to run Vows." >&2
     echo "Try \`make install-dev\` to install Vows." >&2
     exit $ret
 fi
-[ ! -e $testdir ] && \
-    echo "Unable to find directory '$testdir'." >&2 && exit 1
-$vows $@ $testdir/*.js
+$vows $@
